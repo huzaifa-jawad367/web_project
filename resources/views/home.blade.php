649d94d3fe179fb3106abbec39e7e150ec62334b
habibkhan713713 <habibkhan713713@gmail.com>
2023-05-10 03:02:12 +0500

    added nav bars and footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,11 @@
 @endpush
 @section('main')
 <nav class="navbar navbar-expand-lg  navbar-light bg-dark fixed-top">
-    <a class="navbar-brand text-light mr-3  disabled-link"  href="">Home</a>
+    <a class="navbar-brand text-light mr-3  disabled-link"  href="">
+      <svg class=""  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+        <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
+      </svg>
+      Home</a>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item ">
@@ -17,10 +21,10 @@
           <a class="nav-link text-light mr-3" href="#">Log in</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-light mr-3" href="#">Register Lost Child</a>
+            <a class="nav-link text-light mr-3" href="{{url('/')}}/registerc">Report Lost Child</a>
           </li>
         <li class="nav-item">
-          <a class="nav-link text-light mr-3" href="#">Register Found child</a>
+          <a class="nav-link text-light mr-3" href="{{url('/')}}/report">Report Found child</a>
         </li>
       </ul>
       <form method="POST" class="form-inline my-2 w-50">
